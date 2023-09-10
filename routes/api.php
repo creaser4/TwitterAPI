ebe5be42dd9f1461c01c3632c450fdff91f935c4
creaser4 <romasoccedric561@gmail.com>
2023-09-11 03:38:48 +0800

    Optimized API routes

--- routes/api.php
+++ routes/api.php
@@ -25,46 +25,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Authentication Routes
 
-// Login User
-Route::post('/login', [UserController::class, 'login']);
-// Register User
-Route::post('/register', [UserController::class, 'register']);
-//Logout the User and delete Token
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
-
-
-// Create a tweet
-Route::post('/tweets', [TweetController::class, 'store']);
-// Edit a tweet
-Route::put('/tweets/{id}', [TweetController::class, 'edit']);
-// Delete a tweet
-Route::delete('/tweets/{id}', [TweetController::class, 'delete']);
-
-// Protected Routes (require authentication)
-Route::group(['middleware' => ['auth:sanctum']], function() {
-    // Tweet Routes
-    Route::post('/tweets', [TweetController::class, 'store']);
-    Route::put('/tweets/{id}', [TweetController::class, 'edit']);
-    Route::delete('/tweets/{id}',  [TweetController::class, 'delete']);
+Route::controller(UserController::class)->group(function() {
+    Route::post('/login', [UserController::class, 'login']); // Login User
+    Route::post('/register', [UserController::class, 'register']); // Register User
+    Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum'); //Logout the User and delete Token
 });
 
-//User Profile Routes
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
+
     Route::prefix('Follower')->group(function () {
-        // Follow a user
-        Route::post('follow/{user}', [FollowerController::class, 'follow']);
-
-        // Unfollow a user
-        Route::post('unfollow/{user}', [FollowerController::class, 'unfollow']);
-
-        // List of followed users' tweets
-        Route::get('following-tweets', [FollowerController::class, 'following']);
-
-        // List of user's own tweets
-        Route::get('tweets', [FollowerController::class, 'tweets']);
-
-        // Suggested users to follow
-        Route::get('suggested-users', [FollowerController::class, 'suggestedUsers']);
-
+        Route::post('follow/{user}', [FollowerController::class, 'follow']); // Follow a user
+        Route::post('unfollow/{user}', [FollowerController::class, 'unfollow']); // Unfollow a user
+        Route::get('following-tweets', [FollowerController::class, 'following']); // List of followed users' tweets
+        Route::get('tweets', [FollowerController::class, 'tweets']); // List of user's own tweets
+        Route::get('suggested-users', [FollowerController::class, 'suggestedUsers']); // Suggested users to follow
     });
+
+    Route::controller(TweetController::class)->group(function() {
+        Route::post('/tweets', [TweetController::class, 'store']);
+        Route::put('/tweets/{id}', [TweetController::class, 'edit']);
+        Route::delete('/tweets/{id}',  [TweetController::class, 'delete']);
+    });
+    
 });
